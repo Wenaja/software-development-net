85ddae19b78f74694fb19e68ec039511be56418f
rempel <rempel@RempelsNout>
2018-10-16 15:57:02 +0200

    "Teste js"

--- WebContent/WEB-INF/resources/js/validCA.js
+++ WebContent/WEB-INF/resources/js/validCA.js
@@ -1,10 +1,6 @@
 function checkUserName(){
-	var username = document.getElementById("login_form:txt_username").value;
-	alert("Function Ok!");
-	if(username == ""){
-		alert("Benutzername darf nicht leer sein");
-		return false;
-	}
+	var log_form = document.getElementById("login_form");
+	var username = log_form["txt_username"].value;
+	alert("Function Ok! " + username);
 	
-	return true;
 }
